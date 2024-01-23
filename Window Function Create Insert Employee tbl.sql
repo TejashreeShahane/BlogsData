c37e9bcda159c532e03c4ba2a734b72d03146d7f
Tejashree <130588194+TejashreeShahane@users.noreply.github.com>
2024-01-23 18:15:25 -0500

    Final

---
+++ Window Function Create Insert Employee tbl.sql
@@ -0,0 +1,25 @@
+create table Employee
+( EMPLID numeric NOT NULL,
+DEPT VARCHAR(255),
+SALARY numeric)
+
+INSERT INTO Employee values
+(1, 'Sales', 180000),
+(2,'IT',120000),
+(3,'Customer Service',60000),
+(4,'Customer Service',70000),
+(5,'Customer Service',65800),
+(6,'Customer Service',68000),
+(7,'Sales',150000),
+(8,'IT',130000),
+(9,'Sales',125000),
+(10,'IT',121000),
+(11,'Customer Service',72000),
+(12,'Sales',135000),
+(13,'IT',145000),
+(14,'Sales',125000),
+(15,'IT',121000)
+
+SELECT * FROM Employee
+
+commit
